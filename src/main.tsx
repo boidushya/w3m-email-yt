250b15dd37823fc11b8058db78a127ff3a6f0236
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-02-28 19:40:48 +0530

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { WagmiProvider } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const projectId = "2a2a5978a58aad734d13a2d194ec469a"; // Replace with your own project ID from cloud.walletconnect.com
+
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum, polygon] as const;
+
+const wagmiConfig = defaultWagmiConfig({
+  projectId,
+  metadata,
+  chains,
+  enableEmail: true,
+});
+
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
+});
+
+function ContextProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+    </QueryClientProvider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>
+);
